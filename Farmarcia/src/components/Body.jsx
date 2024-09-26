7fcffd5aa986b38e5b5dd5ba704ce117f7f47db6
NICHOLAS SERENCOVICH CARVALHO <nicholas_carvalho@estudante.sesisenai.org.br>
2024-09-26 17:25:38 -0300

    Inicialização do Body

---
+++ Farmarcia/src/components/Body.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import Header from './Header.jsx'
+function Body() {
+  return (
+    <div>
+      
+    <Header/>
+
+    <div className="carrossel">
+
+        <div className="conteudo_carrossel">
+
+            <h2>Cadastre-se na Plataforma</h2>
+            <h3>e faça parte da comunidade!</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum, delectus neque iste est placeat blanditiis non unde rem ipsa laboriosam quia mollitia dolore natus architecto fugit beatae amet animi molestiae. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aut tempora hic eius voluptatem autem praesentium, eaque ipsam quia, officiis facilis sit molestiae doloremque assumenda impedit quod corporis, eos optio quaerat!</p>
+
+            <div className="alinhamento_botao_carrossel">
+                <button>Saiba mais</button>
+            </div>
+        </div>
+
+        <div className="alinhamento_imagem_carrossel">
+            <img src="carrossel_landing_page.png" alt="Imagem carrossel" />
+        </div>
+    </div>
+
+    <div className="cta">
+
+        <img src="farmarcia-cartoes.PNG.png" alt="Imagem de Cartões" />
+
+        <div className="cta_texto">
+
+        <h3>Convites para Inauguração da Farmárcia!</h3>
+
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur fuga a eius ad eos quo possimus beatae aliquam tenetur velit nobis, esse, porro fugit! Veritatis officia ipsam porro officiis fugit!</p>
+
+        <div>
+            <button>Junte-se</button>
+        </div>
+
+        </div>
+
+    </div>
+
+    </div>
+  )
+}
+
+export default Body
---
+++ Farmarcia/src/components/Header.jsx
@@ -0,0 +1,29 @@
+
+function Header() {
+  return (
+    <div>
+
+    <nav>
+
+        <div>
+            <div className="dv_imagem_do_header">
+                <img src="farmarcia-logo-pq.PNG.png" alt="Logo Farmarcia"/>
+
+                <a href="">Início</a>
+                <a href="">Sobre Nós</a>
+                <a href="">Blog</a>
+
+                <div className="dv_botoes_da_nav">
+                    <button className="botao_cadastro">CADASTRAR</button>
+                    <button className="botao_login">LOGIN</button>
+                </div>
+            </div>
+        </div>
+
+    </nav>
+
+    </div>
+  )
+}
+
+export default Header
